3138d5df5389278b20a8b42fe32126c8c3bd10a4
Chertushkin <chertushkinestate@gmail.com>
2024-03-11 00:44:26 -0400

    Add utils

--- utils.py
+++ utils.py
@@ -3,6 +3,123 @@ from typing import Callable, Union, List
 import torch
 from torch.nn.utils.rnn import pad_sequence
 
+
+def tokenize_batch_element(
+    prompt: str, chosen: str, rejected: str, truncation_mode: str, tokenizer, max_length: int, max_prompt_length: int
+) -> Dict:
+    """Tokenize a single batch element.
+
+    At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
+      in case the prompt + chosen or prompt + rejected responses is/are too long. First
+      we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+
+    We also create the labels for the chosen/rejected responses, which are of length equal to
+      the sum of the length of the prompt and the chosen/rejected response, with -100 for the
+      prompt tokens.
+    """
+    chosen_tokens = tokenizer(chosen, add_special_tokens=False)
+    rejected_tokens = tokenizer(rejected, add_special_tokens=False)
+    prompt_tokens = tokenizer(prompt, add_special_tokens=False)
+    if tokenizer.eos_token_id in prompt_tokens["input_ids"]:
+        return None
+    if tokenizer.eos_token_id in chosen_tokens["input_ids"]:
+        return None
+    if tokenizer.eos_token_id in rejected_tokens["input_ids"]:
+        return None
+
+    assert tokenizer.eos_token_id not in prompt_tokens["input_ids"], f"Prompt contains EOS token: {prompt}"
+    assert tokenizer.eos_token_id not in chosen_tokens["input_ids"], f"Chosen response contains EOS token: {chosen}"
+    assert (
+        tokenizer.eos_token_id not in rejected_tokens["input_ids"]
+    ), f"Rejected response contains EOS token: {rejected}"
+
+    chosen_tokens["input_ids"].append(tokenizer.eos_token_id)
+    chosen_tokens["attention_mask"].append(1)
+
+    rejected_tokens["input_ids"].append(tokenizer.eos_token_id)
+    rejected_tokens["attention_mask"].append(1)
+
+    longer_response_length = max(len(chosen_tokens["input_ids"]), len(rejected_tokens["input_ids"]))
+
+    # if combined sequence is too long, truncate the prompt
+    if len(prompt_tokens["input_ids"]) + longer_response_length > max_length:
+        if truncation_mode == "keep_start":
+            prompt_tokens = {k: v[:max_prompt_length] for k, v in prompt_tokens.items()}
+        elif truncation_mode == "keep_end":
+            prompt_tokens = {k: v[-max_prompt_length:] for k, v in prompt_tokens.items()}
+        else:
+            raise ValueError(f"Unknown truncation mode: {truncation_mode}")
+
+    # if that's still too long, truncate the response
+    if len(prompt_tokens["input_ids"]) + longer_response_length > max_length:
+        chosen_tokens = {k: v[: max_length - max_prompt_length] for k, v in chosen_tokens.items()}
+        rejected_tokens = {k: v[: max_length - max_prompt_length] for k, v in rejected_tokens.items()}
+
+    # Create labels
+    chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
+    rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
+    chosen_sequence_tokens["labels"] = chosen_sequence_tokens["input_ids"][:]
+    chosen_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [-100] * len(prompt_tokens["input_ids"])
+    rejected_sequence_tokens["labels"] = rejected_sequence_tokens["input_ids"][:]
+    rejected_sequence_tokens["labels"][: len(prompt_tokens["input_ids"])] = [-100] * len(prompt_tokens["input_ids"])
+
+    batch = {}
+
+    batch["prompt"] = prompt
+    batch["chosen"] = prompt + chosen
+    batch["rejected"] = prompt + rejected
+    batch["chosen_response_only"] = chosen
+    batch["rejected_response_only"] = rejected
+
+    for k, toks in {
+        "chosen": chosen_sequence_tokens,
+        "rejected": rejected_sequence_tokens,
+        "prompt": prompt_tokens,
+    }.items():
+        for type_key, tokens in toks.items():
+            if type_key == "token_type_ids":
+                continue
+            batch[f"{k}_{type_key}"] = tokens
+
+    return batch
+
+
+def get_collate_fn(tokenizer) -> Callable[[List[Dict]], Dict[str, Union[List, torch.Tensor]]]:
+    """Returns a collate function for the given tokenizer.
+
+    The collate function takes a list of examples (dicts, where values are lists of
+      ints [tokens] or strings [the original texts]) and returns a batch of examples,
+      PyTorch tensors padded to the maximum length. Strings are passed through."""
+
+    def collate_fn(batch):
+        # first, pad everything to the same length
+        padded_batch = {}
+        for k in batch[0].keys():
+            if k.endswith("_input_ids") or k.endswith("_attention_mask") or k.endswith("_labels"):
+                if "prompt" in k:  # adapted from https://stackoverflow.com/questions/73256206
+                    to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
+                else:
+                    to_pad = [torch.LongTensor(ex[k]) for ex in batch]
+                if k.endswith("_input_ids"):
+                    padding_value = tokenizer.pad_token_id
+                elif k.endswith("_labels"):
+                    padding_value = -100
+                elif k.endswith("_attention_mask"):
+                    padding_value = 0
+                else:
+                    raise ValueError(f"Unexpected key in batch '{k}'")
+
+                padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
+                if "prompt" in k:  # for the prompt, flip back so padding is on left side
+                    padded_batch[k] = padded_batch[k].flip(dims=[1])
+            else:
+                padded_batch[k] = [ex[k] for ex in batch]
+
+        return padded_batch
+
+    return collate_fn
+
+
 def pad_to_length(tensor: torch.Tensor, length: int, pad_value: Union[int, float], dim: int = -1) -> torch.Tensor:
     if tensor.size(dim) >= length:
         return tensor
@@ -11,108 +128,82 @@ def pad_to_length(tensor: torch.Tensor, length: int, pad_value: Union[int, float
         pad_size[dim] = length - tensor.size(dim)
         return torch.cat([tensor, pad_value * torch.ones(*pad_size, dtype=tensor.dtype, device=tensor.device)], dim=dim)
 
-def tokenize_batch_element(prompt: str, chosen: str, rejected: str, truncation_mode: str, tokenizer, max_length: int, max_prompt_length: int) -> Dict:
-    """Tokenize a single batch element.
 
-       At this stage, we don't convert to PyTorch tensors yet; we just handle the truncation
-         in case the prompt + chosen or prompt + rejected responses is/are too long. First
-         we truncate the prompt; if we're still too long, we truncate the chosen/rejected.
+import torch.nn as nn
 
-       We also create the labels for the chosen/rejected responses, which are of length equal to
-         the sum of the length of the prompt and the chosen/rejected response, with -100 for the
-         prompt tokens.
+
+def concatenated_inputs(batch: Dict[str, Union[List, torch.LongTensor]]) -> Dict[str, torch.LongTensor]:
+    """Concatenate the chosen and rejected inputs into a single tensor.
+
+    Args:
+        batch: A batch of data. Must contain the keys 'chosen_input_ids' and 'rejected_input_ids', which are tensors of shape (batch_size, sequence_length).
+
+    Returns:
+        A dictionary containing the concatenated inputs under the key 'concatenated_input_ids'.
     """
-    chosen_tokens = tokenizer(chosen, add_special_tokens=False)
-    rejected_tokens = tokenizer(rejected, add_special_tokens=False)
-    prompt_tokens = tokenizer(prompt, add_special_tokens=False)
-    if tokenizer.eos_token_id in prompt_tokens['input_ids']:
-        return None
-    if tokenizer.eos_token_id in chosen_tokens['input_ids']:
-        return None
-    if tokenizer.eos_token_id in rejected_tokens['input_ids']:
-        return None
+    max_length = max(batch["chosen_input_ids"].shape[1], batch["rejected_input_ids"].shape[1])
+    concatenated_batch = {}
+    for k in batch:
+        if k.startswith("chosen") and isinstance(batch[k], torch.Tensor):
+            pad_value = -100 if "labels" in k else 0
+            concatenated_key = k.replace("chosen", "concatenated")
+            concatenated_batch[concatenated_key] = pad_to_length(batch[k], max_length, pad_value=pad_value)
+    for k in batch:
+        if k.startswith("rejected") and isinstance(batch[k], torch.Tensor):
+            pad_value = -100 if "labels" in k else 0
+            concatenated_key = k.replace("rejected", "concatenated")
+            concatenated_batch[concatenated_key] = torch.cat(
+                (
+                    concatenated_batch[concatenated_key],
+                    pad_to_length(batch[k], max_length, pad_value=pad_value),
+                ),
+                dim=0,
+            )
+    return concatenated_batch
 
 
-    assert tokenizer.eos_token_id not in prompt_tokens['input_ids'], f"Prompt contains EOS token: {prompt}"
-    assert tokenizer.eos_token_id not in chosen_tokens['input_ids'], f"Chosen response contains EOS token: {chosen}"
-    assert tokenizer.eos_token_id not in rejected_tokens['input_ids'], f"Rejected response contains EOS token: {rejected}"
+def _get_batch_logps(
+    logits: torch.FloatTensor, labels: torch.LongTensor, average_log_prob: bool = False
+) -> torch.FloatTensor:
+    """Compute the log probabilities of the given labels under the given logits.
 
-    chosen_tokens['input_ids'].append(tokenizer.eos_token_id)
-    chosen_tokens['attention_mask'].append(1)
+    Args:
+        logits: Logits of the model (unnormalized). Shape: (batch_size, sequence_length, vocab_size)
+        labels: Labels for which to compute the log probabilities. Label tokens with a value of -100 are ignored. Shape: (batch_size, sequence_length)
+        average_log_prob: If True, return the average log probability per (non-masked) token. Otherwise, return the sum of the log probabilities of the (non-masked) tokens.
 
-    rejected_tokens['input_ids'].append(tokenizer.eos_token_id)
-    rejected_tokens['attention_mask'].append(1)
+    Returns:
+        A tensor of shape (batch_size,) containing the average/sum log probabilities of the given labels under the given logits.
+    """
+    assert logits.shape[:-1] == labels.shape
 
-    longer_response_length = max(len(chosen_tokens['input_ids']), len(rejected_tokens['input_ids']))
+    labels = labels[:, 1:].clone()
+    logits = logits[:, :-1, :]
+    loss_mask = labels != -100
 
-    # if combined sequence is too long, truncate the prompt
-    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
-        if truncation_mode == 'keep_start':
-            prompt_tokens = {k: v[:max_prompt_length] for k, v in prompt_tokens.items()}
-        elif truncation_mode == 'keep_end':
-            prompt_tokens = {k: v[-max_prompt_length:] for k, v in prompt_tokens.items()}
-        else:
-            raise ValueError(f'Unknown truncation mode: {truncation_mode}')
+    # dummy token; we'll ignore the losses on these tokens later
+    labels[labels == -100] = 0
 
-    # if that's still too long, truncate the response
-    if len(prompt_tokens['input_ids']) + longer_response_length > max_length:
-        chosen_tokens = {k: v[:max_length - max_prompt_length] for k, v in chosen_tokens.items()}
-        rejected_tokens = {k: v[:max_length - max_prompt_length] for k, v in rejected_tokens.items()}
+    per_token_logps = torch.gather(logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)).squeeze(2)
 
-    # Create labels
-    chosen_sequence_tokens = {k: prompt_tokens[k] + chosen_tokens[k] for k in chosen_tokens}
-    rejected_sequence_tokens = {k: prompt_tokens[k] + rejected_tokens[k] for k in rejected_tokens}
-    chosen_sequence_tokens['labels'] = chosen_sequence_tokens['input_ids'][:]
-    chosen_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
-    rejected_sequence_tokens['labels'] = rejected_sequence_tokens['input_ids'][:]
-    rejected_sequence_tokens['labels'][:len(prompt_tokens['input_ids'])] = [-100] * len(prompt_tokens['input_ids'])
+    if average_log_prob:
+        return (per_token_logps * loss_mask).sum(-1) / loss_mask.sum(-1)
+    else:
+        return (per_token_logps * loss_mask).sum(-1)
 
-    batch = {}
 
-    batch['prompt'] = prompt
-    batch['chosen'] = prompt + chosen
-    batch['rejected'] = prompt + rejected
-    batch['chosen_response_only'] = chosen
-    batch['rejected_response_only'] = rejected
+def concatenated_forward(
+    model: nn.Module, batch: Dict[str, Union[List, torch.LongTensor]]
+) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+    """Run the given model on the given batch of inputs, concatenating the chosen and rejected inputs together.
 
-    for k, toks in {'chosen': chosen_sequence_tokens, 'rejected': rejected_sequence_tokens, 'prompt': prompt_tokens}.items():
-        for type_key, tokens in toks.items():
-            if type_key == 'token_type_ids':
-                continue
-            batch[f'{k}_{type_key}'] = tokens
-
-    return batch
-
-def get_collate_fn(tokenizer) -> Callable[[List[Dict]], Dict[str, Union[List, torch.Tensor]]]:
-    """Returns a collate function for the given tokenizer.
-
-       The collate function takes a list of examples (dicts, where values are lists of
-         ints [tokens] or strings [the original texts]) and returns a batch of examples,
-         PyTorch tensors padded to the maximum length. Strings are passed through."""
-    def collate_fn(batch):
-        # first, pad everything to the same length
-        padded_batch = {}
-        for k in batch[0].keys():
-            if k.endswith('_input_ids') or k.endswith('_attention_mask') or k.endswith('_labels'):
-                if 'prompt' in k:  # adapted from https://stackoverflow.com/questions/73256206
-                    to_pad = [torch.LongTensor(ex[k][::-1]) for ex in batch]
-                else:
-                    to_pad = [torch.LongTensor(ex[k]) for ex in batch]
-                if k.endswith('_input_ids'):
-                    padding_value = tokenizer.pad_token_id
-                elif k.endswith('_labels'):
-                    padding_value = -100
-                elif k.endswith('_attention_mask'):
-                    padding_value = 0
-                else:
-                    raise ValueError(f"Unexpected key in batch '{k}'")
-
-                padded_batch[k] = pad_sequence(to_pad, batch_first=True, padding_value=padding_value)
-                if 'prompt' in k:  # for the prompt, flip back so padding is on left side
-                    padded_batch[k] = padded_batch[k].flip(dims=[1])
-            else:
-                padded_batch[k] = [ex[k] for ex in batch]
-
-        return padded_batch
-    return collate_fn
-# tokenize_batch_element(ex[0], ex[0], ex[1], 'keep_start', tokenizer, 256, 256)
+    We do this to avoid doing two forward passes, because it's faster for FSDP.
+    """
+    concatenated_batch = concatenated_inputs(batch)
+    all_logits = model(
+        concatenated_batch["concatenated_input_ids"], attention_mask=concatenated_batch["concatenated_attention_mask"]
+    ).logits.to(torch.float32)
+    all_logps = _get_batch_logps(all_logits, concatenated_batch["concatenated_labels"], average_log_prob=False)
+    chosen_logps = all_logps[: batch["chosen_input_ids"].shape[0]]
+    rejected_logps = all_logps[batch["chosen_input_ids"].shape[0] :]
+    return chosen_logps, rejected_logps
